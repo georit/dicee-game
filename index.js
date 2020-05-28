c804855224c4daff5ccb9f21a0c417f3b178ba3d
George Itai <65042575+georit@users.noreply.github.com>
2020-05-28 16:41:33 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-                    // ***************************** METHOD TWO ************************************
+                    // ***************************** METHOD ONE ************************************
 
 var diceImages = ["images/dice1.png", "images/dice2.png", "images/dice3.png", "images/dice4.png", "images/dice5.png", "images/dice6.png"];
 var randomNumber1 = Math.floor(Math.random() * diceImages.length);
